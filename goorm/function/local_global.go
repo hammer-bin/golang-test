91cd104604578e96366408907e5ec7f3685260c8
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-07 06:32:08 +0900

    add to function

---
+++ goorm/function/local_global.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func exampleFunc1() {
+	var a int = 10
+
+	a++
+
+	fmt.Println("exampleFunc1의 a는 ", a)
+}
+
+func exampleFunc2() {
+	var b int = 20
+	var c int = 30
+
+	b++
+	c++
+
+	fmt.Println("b와 c는 ", b, c)
+}
+
+func main() {
+	var a int = 28
+
+	exampleFunc1()
+	exampleFunc2()
+	fmt.Println("main의 a는", a)
+}
