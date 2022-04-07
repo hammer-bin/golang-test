36b10bb2bb360e83b356ff4cd2bfbcd1975ac1ea
suslmk-lee <67575226+suslmk-lee@users.noreply.github.com>
2022-04-07 21:06:13 +0900

    commit

---
+++ goorm/function/exam01/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func bubbleSort(r []int) {
+	var temp int
+	for i := len(r) - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if r[j] < r[j+1] {
+				temp = r[j]
+				r[j] = r[j+1]
+				r[j+1] = temp
+			}
+		}
+	}
+}
+
+func inputNums() []int {
+	var count, num int
+	var slice []int
+	fmt.Scanf("%d", &count)
+	for i := 0; i < count; i++ {
+		fmt.Scanf("%d\n", &num)
+		slice = append(slice, num)
+	}
+	return slice
+}
+
+func outputNums(r []int) {
+	for i := 0; i < len(r); i++ {
+		fmt.Printf("%d ", r[i])
+	}
+}
+
+func main() {
+	nums := inputNums()
+	bubbleSort(nums)
+	outputNums(nums)
+}
