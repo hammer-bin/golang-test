5e2c219a9f3c534491842832ce8d365e87de88aa
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-18 06:43:22 +0900

    add to html

--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.14
 
 require (
 	github.com/go-resty/resty/v2 v2.7.0
+	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/pat v1.0.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/vault/api v1.5.0 // indirect
 	github.com/slack-go/slack v0.10.2
 	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/tuckersGo/goWeb v0.0.0-20200531032145-28d77cfd0a29 // indirect
+	github.com/unrolled/render v1.4.1 // indirect
 )
--- turkerweb/WEB12/main.go
+++ turkerweb/WEB12/main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/pat"
+	"github.com/unrolled/render"
+	"net/http"
+	"time"
+)
+
+var rd *render.Render
+
+type User struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{Name: "suslmk", Email: "suslmk@naver.com"}
+
+	rd.JSON(w, http.StatusOK, user)
+	/*w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))*/
+
+}
+
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		/*w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)*/
+		rd.Text(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	user.CreatedAt = time.Now()
+	rd.JSON(w, http.StatusOK, user)
+	/*w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))*/
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	/*tmpl, err := template.New("Hello").ParseFiles("D:/workspaceGolang/golang-test/turkerweb/WEB12/templates/hello.tmpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		rd.Text(w, http.StatusInternalServerError, err.Error())
+		return
+	}*/
+	rd.HTML(w, http.StatusOK, "hello", "Suslmk")
+	//tmpl.ExecuteTemplate(w, "hello.tmpl", "Suslmk")
+}
+
+func main() {
+	rd = render.New(render.Options{
+		Extensions: []string{".html", ".tmpl"},
+	})
+	mux := pat.New()
+
+	mux.Get("/users", getUserInfoHandler)
+	mux.Post("/users", addUserHandler)
+	mux.Get("/hello", helloHandler)
+
+	//mux.HandleFunc("/users", getUserInfoHandler).Methods("GET")
+	//mux.HandleFunc("/users", addUserHandler).Methods("POST")
+
+	http.ListenAndServe(":3000", mux)
+}
