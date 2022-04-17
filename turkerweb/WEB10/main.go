b31a07d91fa4256ca83a135e0dabeaf7f20eb2a5
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-17 15:35:22 +0900

    add to deco

--- turkerweb/WEB10/main.go
+++ turkerweb/WEB10/main.go
@@ -1,1 +1,36 @@
-package WEB10
+package main
+
+import (
+	"golang-test/test01/example01/turkerweb/WEB10/myapp"
+	"golang-test/test01/example01/turkerweb/WEB5/decoHandler"
+	"log"
+	"net/http"
+	"time"
+)
+
+func logger(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Print("[LOGGER1] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER1] Completed time:", time.Since(start).Milliseconds())
+}
+
+func logger2(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Print("[LOGGER2] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER2] Completed time:", time.Since(start).Milliseconds())
+}
+
+func NewHandler() http.Handler {
+	h := myapp.NewHandler()
+	h = decoHandler.NewDecoHandler(h, logger)
+	h = decoHandler.NewDecoHandler(h, logger2)
+	return h
+}
+
+func main() {
+	mux := myapp.NewHandler()
+
+	http.ListenAndServe(":3000", mux)
+}
