51e398ca686370f749ebef04aa00f7578f237734
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-20 06:16:24 +0900

    add to refectoring

--- turkerweb/WEB16/app/app.go
+++ turkerweb/WEB16/app/app.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"WEB16/model"
 	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
 	"net/http"
+	"strconv"
 )
 
 var rd *render.Render
@@ -13,12 +16,55 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func getTodoListHandler(w http.ResponseWriter, r *http.Request) {
 
+	list := model.GetTodos()
+	rd.JSON(w, http.StatusOK, list)
+}
+
+func addTodoHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	todo := model.AddTodo(name)
+
+	rd.JSON(w, http.StatusCreated, todo)
+}
+
+type Success struct {
+	Success bool `json:"success"`
+}
+
+func removeTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	ok := model.RemoveTodo(id)
+	if ok {
+		rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		rd.JSON(w, http.StatusOK, Success{false})
+	}
+
+}
+
+func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	complete := r.FormValue("complete") == "true"
+	ok := model.CompleteTodo(id, complete)
+	if ok {
+		rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		rd.JSON(w, http.StatusOK, Success{false})
+	}
+
 }
 
 func MakeHandler() http.Handler {
+	//todoMap = make(map[int]*Todo)
+	rd = render.New()
 	r := mux.NewRouter()
 
 	r.HandleFunc("/todos", getTodoListHandler).Methods("GET")
+	r.HandleFunc("/todos", addTodoHandler).Methods("POST")
+	r.HandleFunc("/todos/{id:[0-9]+}", removeTodoHandler).Methods("DELETE")
+	r.HandleFunc("/complete-todo/{id:[0-9]+}", completeTodoHandler).Methods("GET")
 	r.HandleFunc("/", indexHandler)
 
 	return r
