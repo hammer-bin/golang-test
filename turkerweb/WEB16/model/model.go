5b1da1c9fad5a83f53a4f357ee5ef21433c86874
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-20 06:54:42 +0900

    add to interface

--- turkerweb/WEB16/model/model.go
+++ turkerweb/WEB16/model/model.go
@@ -11,40 +11,72 @@ type Todo struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-var todoMap map[int]*Todo
-
-func init() {
-	todoMap = make(map[int]*Todo)
+type dbHandler interface {
+	getTodos() []*Todo
+	addTodo(name string) *Todo
+	removeTodo(id int) bool
+	completeTodo(id int, complete bool) bool
 }
 
-func GetTodos() []*Todo {
+// MemoryHandler는 dbHandler인터페이스 임플리먼트(구현) 하고 있다.
+// 그래서 dbHandler만 들고 있으면 된다. (61 line)
+type memoryHandler struct {
+	todoMap map[int]*Todo
+}
+
+func (m *memoryHandler) getTodos() []*Todo {
 	list := []*Todo{}
-	for _, v := range todoMap {
+	for _, v := range m.todoMap {
 		list = append(list, v)
 	}
 	return list
 }
-
-func AddTodo(name string) *Todo {
+func (m *memoryHandler) addTodo(name string) *Todo {
 	id := len(todoMap) + 1
 	todo := &Todo{id, name, false, time.Now()}
-	todoMap[id] = todo
+	m.todoMap[id] = todo
 	return todo
 }
-
-func RemoveTodo(id int) bool {
-	if _, ok := todoMap[id]; ok {
-		delete(todoMap, id)
+func (m *memoryHandler) removeTodo(id int) bool {
+	if _, ok := m.todoMap[id]; ok {
+		delete(m.todoMap, id)
 		return true
 	}
 	return false
-
 }
-
-func CompleteTodo(id int, complete bool) bool {
-	if todo, ok := todoMap[id]; ok {
+func (m *memoryHandler) completeTodo(id int, complete bool) bool {
+	if todo, ok := m.todoMap[id]; ok {
 		todo.Completed = complete
 		return true
 	}
 	return false
+}
+
+func newMemoryHandler() dbHandler {
+	m := &memoryHandler{}
+	m.todoMap = make(map[int]*Todo)
+	return m
+}
+
+var handler dbHandler
+
+func init() {
+	handler = newMemoryHandler()
+	todoMap = make(map[int]*Todo)
+}
+
+func GetTodos() []*Todo {
+	return handler.getTodos()
+}
+
+func AddTodo(name string) *Todo {
+	return handler.addTodo(name)
+}
+
+func RemoveTodo(id int) bool {
+	return handler.removeTodo(id)
+}
+
+func CompleteTodo(id int, complete bool) bool {
+	return handler.completeTodo(id, complete)
 }
