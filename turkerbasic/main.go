9fbc54fbe076709eca9455d6e58aaf75e158a634
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-26 06:06:48 +0900

    add to BST

--- turkerbasic/main.go
+++ turkerbasic/main.go
@@ -28,4 +28,37 @@ func main() {
 	fmt.Println()
 
 	tree.DFS2()
+	fmt.Println()
+
+	tree.BFS()
+
+	fmt.Println()
+
+	tree1 := dataStruct.NewBinaryTree(5)
+	tree1.Root.AddNode(3)
+	tree1.Root.AddNode(2)
+	tree1.Root.AddNode(4)
+	tree1.Root.AddNode(8)
+	tree1.Root.AddNode(7)
+	tree1.Root.AddNode(6)
+	tree1.Root.AddNode(10)
+	tree1.Root.AddNode(9)
+
+	tree1.Print()
+	fmt.Println()
+
+	if found, cnt := tree1.Search(6); found {
+		fmt.Println("found 6 cnt", cnt)
+	} else {
+		fmt.Println("not found 6 cnt ", cnt)
+	}
+
+	fmt.Println()
+
+	if found, cnt := tree1.Search(11); found {
+		fmt.Println("found 11 cnt", cnt)
+	} else {
+		fmt.Println("not found 11 cnt ", cnt)
+	}
+
 }
