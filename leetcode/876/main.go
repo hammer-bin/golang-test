5b5df833c2a0c3c03b9ab183c0211a037c49fafb
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-04-30 07:16:56 +0900

    mission success leetcode 876

--- leetcode/876/main.go
+++ leetcode/876/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 /**
 876. Middle of the Linked List
  * Definition for singly-linked list.
@@ -15,10 +19,78 @@ Input: head = [1,2,3,4,5,6]
 Output: [4,5,6]
 Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
 */
-func middleNode(head *ListNode) *ListNode {
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type LinkedList struct {
+	head *ListNode
+	tail *ListNode
+}
+
+func (l *LinkedList) addNode(val int) {
+
+	if l.head == nil {
+		l.head = &ListNode{Val: val}
+		l.tail = l.head
+		return
+	}
+	l.tail.Next = &ListNode{Val: val}
+	l.tail = l.tail.Next
+}
+
+func (l *LinkedList) printNode() {
+	node := l.head
+	for node.Next != nil {
+		fmt.Printf("%d -> ", node.Val)
+		node = node.Next
+	}
+	fmt.Printf("%d -> ", node.Val)
+	fmt.Println()
 
 }
 
-func main() {
+func middleNode(head *ListNode) *ListNode {
+	cnt := 0
+	node := head
+	for node.Next != nil {
+		cnt++
+		node = node.Next
+	}
+	cnt++
+	fmt.Println("cnt : ", cnt)
 
+	node1 := head
+	for i := 0; i < cnt/2; i++ {
+		node1 = node1.Next
+	}
+	rstNode := node1
+
+	return rstNode
+}
+
+func main() {
+	sl := []int{1, 2, 3, 4, 5, 6}
+	list := &LinkedList{}
+	for _, v := range sl {
+		list.addNode(v)
+	}
+	list.printNode()
+
+	node := middleNode(list.head)
+
+	fmt.Println()
+
+	printNode(node)
+
+}
+
+func printNode(node *ListNode) {
+	for node.Next != nil {
+		fmt.Printf("%d -> ", node.Val)
+		node = node.Next
+	}
+	fmt.Printf("%d -> ", node.Val)
 }
