d91a7bdfb002371a72f13f0e4cea06c4e5e3df3f
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-05-07 15:52:36 +0900

    add to dataStruct1

--- turkerbasic/repeat1/main.go
+++ turkerbasic/repeat1/main.go
@@ -34,7 +34,7 @@ func mainLink() {
 }
 
 func main() {
-	var stack []int
+	/*var stack []int
 
 	for i := 1; i <= 5; i++ {
 		stack = append(stack, i)
@@ -87,4 +87,70 @@ func main() {
 		fmt.Printf("%d ->", val)
 	}
 
+	fmt.Println()
+
+	//tree
+	tree := dataStruct1.Tree{}
+
+	val := 1
+	tree.AddNode(val)
+	val++
+
+	for i := 0; i < 3; i++ {
+		tree.Root.AddNode(val)
+		val++
+	}
+
+	for i := 0; i < len(tree.Root.Childs); i++ {
+		for j := 0; j < 2; j++ {
+			tree.Root.Childs[i].AddNode(val)
+			val++
+		}
+	}
+
+	tree.DFS1()
+	fmt.Println()
+	tree.DFS2()
+	fmt.Println()
+	tree.BFS()
+	*/
+
+	/*tree := dataStruct1.NewBinaryTree(5)
+	tree.Root.AddNode(3)
+	tree.Root.AddNode(2)
+	tree.Root.AddNode(4)
+	tree.Root.AddNode(8)
+	tree.Root.AddNode(7)
+	tree.Root.AddNode(6)
+	tree.Root.AddNode(10)
+	tree.Root.AddNode(9)
+
+	tree.Print()
+	fmt.Println()
+	if found, cnt := tree.Search(6); found {
+		fmt.Println("found 6 cnt: ", cnt)
+	} else {
+		fmt.Println("not found 6 cnt: ", cnt)
+	}
+
+	if found, cnt := tree.Search(11); found {
+		fmt.Println("found 11 cnt: ", cnt)
+	} else {
+		fmt.Println("not found 11 cnt: ", cnt)
+	}*/
+
+	h := dataStruct1.Heap{}
+
+	h.Push(2)
+	h.Push(6)
+	h.Push(9)
+	h.Push(6)
+	h.Push(7)
+	h.Push(8)
+	h.Print()
+
+	fmt.Println(h.Pop())
+	fmt.Println(h.Pop())
+	fmt.Println(h.Pop())
+	fmt.Println(h.Pop())
 }
